3d439196cc9d57acc99bf594a579949f860dc46c
Tobias <tobias.loveridge@gmail.com>
2025-01-23 01:15:49 +0000

    Added foliage spawning via an event.

--- src/world_setup.rs
+++ src/world_setup.rs
@@ -1,9 +1,27 @@
 use bevy::prelude::*;
 use noise::{NoiseFn, Perlin};
 
+#[derive(Component)]
+pub struct WaterVoxel;
+
+#[derive(Component)]
+pub struct SandVoxel;
+
+#[derive(Component)]
+pub struct GrassVoxel;
+
+#[derive(Component)]
+pub struct Foliage;
+
+
+#[derive(Event)]
+pub struct VoxelsSpawnedEvent;
+
 pub(super) fn plugin(app: &mut App) {
     app
-        .add_systems(Startup, spawn_world);
+        .add_event::<VoxelsSpawnedEvent>()
+        .add_systems(Startup, spawn_world)
+        .add_systems(Update, generate_foliage);
 }
 
 
@@ -11,6 +29,7 @@ pub fn spawn_world(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    mut event_writer: EventWriter<VoxelsSpawnedEvent>,
 ) {
     let world_mesh = Mesh3d(meshes.add(Plane3d {
         half_size: Vec2::new(30.0, 30.0),
@@ -73,22 +92,66 @@ pub fn spawn_world(
 
             if p_value < 0.0 {
                 commands.spawn((
+                    WaterVoxel,
                     Mesh3d(voxel.clone()),
                     MeshMaterial3d(water_voxel_mat.clone()),
                     Transform::from_translation(Vec3::new(i as f32, -1., j as f32)),
                 ));
             } else if p_value >= 0.0 && p_value < 0.15 {
                 commands.spawn((
+                    SandVoxel,
                     Mesh3d(voxel.clone()),
                     MeshMaterial3d(sand_voxel_mat.clone()),
                     Transform::from_translation(Vec3::new(i as f32, -0.85, j as f32)),
                 ));
             } else {
                 commands.spawn((
+                    GrassVoxel,
                     Mesh3d(voxel.clone()),
                     MeshMaterial3d(grass_voxel_mat.clone()),
                     Transform::from_translation(Vec3::new(i as f32, -0.8, j as f32)),
                 ));
+            }
+        }
+    }
+
+    event_writer.send(VoxelsSpawnedEvent);
+}
+
+fn generate_foliage(
+    mut commands: Commands,
+    mut events: EventReader<VoxelsSpawnedEvent>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut grass_voxel_query: Query<(Entity, &Transform), With<GrassVoxel>>,
+) {
+
+    for active_event in events.read() {
+        match active_event {
+            VoxelsSpawnedEvent => {
+                
+                println!("Event received!");
+
+                let foliage = Mesh3d(meshes.add(Cuboid {
+                    half_size: Vec3::new(0.1, 0.3, 0.1),
+                    ..default()
+                }));
+
+                let foliage_mat = MeshMaterial3d(materials.add(StandardMaterial {
+                    base_color: Color::linear_rgb(0.1, 0.9,0.4),
+                    ..default()
+                }));
+
+                for (_entity, transform) in grass_voxel_query.iter_mut() {
+                    if rand::random::<f32>() < 0.2 {
+                        commands.spawn((
+                            Foliage,
+                            foliage.clone(),
+                            foliage_mat.clone(),
+                            Transform::from_translation(transform.translation + Vec3::new(0.0, 0.75, 0.0)),
+                        ));
+                    }
+                }
             }
         }
     }
